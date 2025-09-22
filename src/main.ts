11d8c0efac069109d4feae98ad9a9ca2a424b80c
Ahmed ZAghloul <zaghloul85@gmail.com>
2025-09-22 15:23:17 +0300

    add debugging

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   const port = process.env.PORT ?? 3000;
   app.use(helmet());
   app.enableCors({
-    origin: '*',
+    origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: [
