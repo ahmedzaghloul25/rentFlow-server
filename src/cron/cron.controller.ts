c39c6f4480aeabbdd5e6e77aaef1bc42af33662d
Ahmed ZAghloul <zaghloul85@gmail.com>
2025-09-23 22:22:41 +0300

    update cron jobs methods

--- src/cron/cron.controller.ts
+++ src/cron/cron.controller.ts
@@ -1,31 +1,27 @@
-import { Controller, Headers, Post, UnauthorizedException } from '@nestjs/common';
+import { Controller, Get, Headers, UnauthorizedException } from '@nestjs/common';
 import { CronService } from './cron.service';
 
 @Controller('cron')
 export class CronController {
     constructor(private cronService: CronService) { }
 
-    @Post('terminate-contracts')
+    @Get('terminate-contracts')
     async terminateExpiredContracts(@Headers('authorization') authHeader: string
-    ) {
+    ) {        
         const cronSecret = process.env.CRON_SECRET;
-
         if (`Bearer ${cronSecret}` !== authHeader) {
             throw new UnauthorizedException('Invalid cron secret.');
         }
-
         return await this.cronService.autoTerminateContracts();
     }
 
-    @Post('create-payments')
+    @Get('create-payments')
     async createPaymentRecords(@Headers('authorization') authHeader: string
     ) {
         const cronSecret = process.env.CRON_SECRET;
-
         if (`Bearer ${cronSecret}` !== authHeader) {
             throw new UnauthorizedException('Invalid cron secret.');
         }
-
         return await this.cronService.autoCreateRemainingPayments();
     }
 }
